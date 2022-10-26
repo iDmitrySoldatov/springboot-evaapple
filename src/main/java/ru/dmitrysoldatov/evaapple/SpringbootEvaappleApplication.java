fb40ff352a8f3ca5955aed55246acb71dad90fb9
dmitry <dmitriysoldatov1998@gmail.com>
2022-10-26 10:36:02 +0500

    Create project

---
+++ src/main/java/ru/dmitrysoldatov/evaapple/SpringbootEvaappleApplication.java
@@ -0,0 +1,13 @@
+package ru.dmitrysoldatov.evaapple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEvaappleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEvaappleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dmitrysoldatov/evaapple/SpringbootEvaappleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmitrysoldatov.evaapple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEvaappleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
