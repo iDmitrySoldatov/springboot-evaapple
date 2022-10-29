ab83518d38a3d9a45b8ac22046a379b156127b76
dmitry <dmitriysoldatov1998@gmail.com>
2022-10-29 14:27:32 +0500

    add swagger

--- src/main/java/ru/dmitrysoldatov/evaapple/SpringbootEvaappleApplication.java
+++ src/main/java/ru/dmitrysoldatov/evaapple/SpringbootEvaappleApplication.java
@@ -4,8 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableSwagger2
 public class SpringbootEvaappleApplication {
 
 	public static void main(String[] args) {
@@ -16,6 +21,5 @@ public class SpringbootEvaappleApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-
 
 }
--- src/main/java/ru/dmitrysoldatov/evaapple/controllers/CategoriesController.java
+++ src/main/java/ru/dmitrysoldatov/evaapple/controllers/CategoriesController.java
@@ -1,23 +1,9 @@
 package ru.dmitrysoldatov.evaapple.controllers;
-
-
 import org.springframework.web.bind.annotation.*;
-import ru.dmitrysoldatov.evaapple.models.Categories;
-import ru.dmitrysoldatov.evaapple.repositories.CategoriesRepository;
+
 
 @RestController
 @RequestMapping("/categories")
 public class CategoriesController {
-
-    private final CategoriesRepository repository;
-
-    public CategoriesController(CategoriesRepository repository){
-        this.repository = repository;
-    }
-
-    @GetMapping("/test")
-    public Categories get(@RequestParam int id){
-       return this.repository.findById(id).get();
-    }
 
 }
