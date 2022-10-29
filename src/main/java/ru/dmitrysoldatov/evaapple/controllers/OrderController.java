f359cd61c7a033b28d9dea198ffec8a9dffc6dd4
dmitry <dmitriysoldatov1998@gmail.com>
2022-10-29 23:39:41 +0500

    add order and product controller

--- src/main/java/ru/dmitrysoldatov/evaapple/controllers/OrderController.java
+++ src/main/java/ru/dmitrysoldatov/evaapple/controllers/OrderController.java
@@ -1,4 +1,73 @@
 package ru.dmitrysoldatov.evaapple.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.dmitrysoldatov.evaapple.dto.OrderDTO;
+import ru.dmitrysoldatov.evaapple.exception.ResourceNotFoundException;
+import ru.dmitrysoldatov.evaapple.services.OrderService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class OrderController {
+    private OrderService service;
+
+    @Autowired
+    public OrderController(OrderService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/order")
+    public List<OrderDTO> getAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/order/{id}")
+    public ResponseEntity<OrderDTO> getOrder(@PathVariable Integer id) {
+        OrderDTO orderDTO = service.findById(id);
+        if (orderDTO == null) {
+            throw new ResourceNotFoundException("Order not exist with id :" + id);
+        } else {
+            return ResponseEntity.ok(orderDTO);
+        }
+    }
+
+    @PostMapping("/order")
+    public OrderDTO createOrder(@RequestBody OrderDTO orderDTO) {
+        return service.save(orderDTO);
+    }
+
+    @PutMapping("/order/{id}")
+    public ResponseEntity<OrderDTO> updateOrder(@PathVariable Integer id,
+                                                          @RequestBody OrderDTO orderDTO) {
+        OrderDTO order = service.findById(id);
+        if (order == null) {
+            throw new ResourceNotFoundException("Order not exist with id :" + id);
+        } else {
+            order.setPhoneNumber(order.getPhoneNumber());
+            order.setClienName(order.getClienName());
+            order.setCity(order.getCity());
+            order.setDateTime(order.getDateTime());
+            order.setProduct(order.getProduct());
+
+            OrderDTO upadateOrder = service.save(order);
+            return ResponseEntity.ok(upadateOrder);
+        }
+    }
+
+    @DeleteMapping("/order/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteOrder(@PathVariable Integer id) {
+        OrderDTO order = service.findById(id);
+        if (order == null) {
+            throw new ResourceNotFoundException("Order not exist with id :" + id);
+        } else {
+            service.deleteById(id);
+            Map<String, Boolean> response = new HashMap<>();
+            response.put("deleted", Boolean.TRUE);
+            return ResponseEntity.ok(response);
+        }
+    }
 }
--- src/main/java/ru/dmitrysoldatov/evaapple/controllers/ProductController.java
+++ src/main/java/ru/dmitrysoldatov/evaapple/controllers/ProductController.java
@@ -1,4 +1,74 @@
 package ru.dmitrysoldatov.evaapple.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.dmitrysoldatov.evaapple.dto.ProductDTO;
+import ru.dmitrysoldatov.evaapple.exception.ResourceNotFoundException;
+import ru.dmitrysoldatov.evaapple.services.ProductService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class ProductController {
+    private ProductService service;
+
+    @Autowired
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/product")
+    public List<ProductDTO> getAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable Integer id) {
+        ProductDTO productDTO = service.findById(id);
+        if (productDTO == null) {
+            throw new ResourceNotFoundException("Product not exist with id :" + id);
+        } else {
+            return ResponseEntity.ok(productDTO);
+        }
+    }
+
+    @PostMapping("/product")
+    public ProductDTO createProduct(@RequestBody ProductDTO productDTO) {
+        return service.save(productDTO);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Integer id,
+                                                @RequestBody ProductDTO productDTO) {
+        ProductDTO product = service.findById(id);
+        if (product == null) {
+            throw new ResourceNotFoundException("Product not exist with id :" + id);
+        } else {
+            product.setName(productDTO.getName());
+            product.setDescription(productDTO.getDescription());
+            product.setPrice(productDTO.getPrice());
+            product.setCategories(productDTO.getCategories());
+            product.setOrderList(productDTO.getOrderList());
+            product.setImagesURL(productDTO.getImagesURL());
+
+            ProductDTO upadateProduct = service.save(product);
+            return ResponseEntity.ok(upadateProduct);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteProduct(@PathVariable Integer id) {
+        ProductDTO prduct = service.findById(id);
+        if (prduct == null) {
+            throw new ResourceNotFoundException("Order not exist with id :" + id);
+        } else {
+            service.deleteById(id);
+            Map<String, Boolean> response = new HashMap<>();
+            response.put("deleted", Boolean.TRUE);
+            return ResponseEntity.ok(response);
+        }
+    }
 }
