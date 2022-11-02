d6fc442f70958bbc459a3c81443affa1d68a50fb
Dmitry Soldatov <dmitriysoldatov1998@gmail.com>
2022-11-02 17:07:12 +0500

    add loadimages in ImageStorage and ImageStorageImp

--- src/main/java/ru/dmitrysoldatov/evaapple/Image/ImageStorage.java
+++ src/main/java/ru/dmitrysoldatov/evaapple/Image/ImageStorage.java
@@ -4,4 +4,6 @@ import java.util.List;
 
 public interface ImageStorage {
     List<String> getImages(String entity, Integer entityId);
+    void loadImages(String entity, Integer id,String imagesName, byte[] imagesBytes);
+    void loadImages(String entity, Integer id,String imagesName, String URL);
 }
--- src/main/java/ru/dmitrysoldatov/evaapple/Image/ImageStorageImp.java
+++ src/main/java/ru/dmitrysoldatov/evaapple/Image/ImageStorageImp.java
@@ -2,8 +2,13 @@ package ru.dmitrysoldatov.evaapple.Image;
 
 import org.springframework.stereotype.Component;
 
-import java.io.File;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +29,48 @@ public class ImageStorageImp implements ImageStorage{
         return images;
         }catch (NullPointerException e) {
             return new ArrayList<>();
+        }
+    }
+    @Override
+    public void loadImages(String entity, Integer id,String imagesName, byte[] imagesBytes) {
+        Path file = Paths.get(path);
+        if (!Files.exists(file)){
+            try {
+                Files.createDirectories(file);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Path newFile = Paths.get(path,imagesName + ".jpg");
+        try (FileOutputStream fileOutputStream = new FileOutputStream(newFile.toFile())){
+            fileOutputStream.write(imagesBytes);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    public void loadImages(String entity, Integer id,String imagesName, String URL) {
+        Path file = Paths.get(path);
+        if(!Files.exists(file)) {
+            try {
+                Files.createDirectories(file);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Path newFile =Paths.get(path, imagesName + ".jpg");
+        java.net.URL url = null;
+        try {
+            url = new URL(URL);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        try (InputStream inputStream = url.openStream()){
+            Files.copy(inputStream, newFile, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
